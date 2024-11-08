8ca04dd59564da760576afd77a59a176be69fda7
Nelson <favournelson82@gmail.com>
2024-11-08 16:51:09 +0100

    Nav Component created

--- Client/src/Components/Nav.tsx
+++ Client/src/Components/Nav.tsx
@@ -1,24 +1,20 @@
-
+import '../index.css'
 
 function Nav() {
   return (
-   <section>
-    <header>
-        <nav>
-            <ul>
-                <h1>RsvIt</h1>
-            </ul>
-            <ul className="">
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-            </ul>
-            <ul>
-                <li><button>Make A Reservation</button></li>
-            </ul>
-        </nav>
-    </header>
-   </section>
-  )
+    <section className='container'>
+      <nav className="flex justify-between items-center">
+        <h1 className="text-lg font-bold">RsvIt</h1>
+        <ul className="flex space-x-4">
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+        </ul>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Make A Reservation
+        </button>
+      </nav>
+    </section>
+  );
 }
 
-export default Nav
+export default Nav;
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize:{
+        custom:'20rem'
+      },
+    },
   },
   plugins: [],
-}
-
+};
