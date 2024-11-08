4a1d9bdaadbab9b3d0aef334a85c1cdff50d5e01
Nelson <favournelson82@gmail.com>
2024-11-08 16:20:37 +0100

    Initial Commit

---
+++ Client/src/Components/Nav.tsx
@@ -0,0 +1,24 @@
+
+
+function Nav() {
+  return (
+   <section>
+    <header>
+        <nav>
+            <ul>
+                <h1>RsvIt</h1>
+            </ul>
+            <ul className="">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+            </ul>
+            <ul>
+                <li><button>Make A Reservation</button></li>
+            </ul>
+        </nav>
+    </header>
+   </section>
+  )
+}
+
+export default Nav
---
+++ Client/src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Nav from "../../Components/Nav"
+
+function Home() {
+  return (
+    <section>
+        <main>
+      <Nav/>
+        </main>
+    </section>
+  )
+}
+
+export default Home
