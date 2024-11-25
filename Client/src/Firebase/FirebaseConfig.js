2090cd36f942269c7844eecc29215c3a6e0db1f2
Nelson <favournelson82@gmail.com>
2024-11-25 04:11:09 +0100

    responsive layout

--- Client/src/Firebase/FirebaseConfig.js
+++ Client/src/Firebase/FirebaseConfig.js
@@ -1,18 +1,15 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBGiwWPvEXSIuh_i0Dft3_3SQzXF5GC4LA',
-//   authDomain: 'dine-restaurant-f4739.firebaseapp.com',
-//   projectId: 'dine-restaurant-f4739',
-//   storageBucket: 'dine-restaurant-f4739.firebasestorage.app',
-//   messagingSenderId: '963326269079',
-//   appId: '1:963326269079:web:d841de97611ae9f154fbd2',
-//   measurementId: 'G-86YD6DMJLP',
-// };
+ apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 
 
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
