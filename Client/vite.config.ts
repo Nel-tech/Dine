4c9e9dd092758753a354ef40e21f929a7e5de470
Nelson <favournelson82@gmail.com>
2024-12-02 02:58:31 +0100

    changed img location

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
-})
+  base: '/', // Adjust if you're deploying to a subdirectory
+  build: {
+    outDir: 'dist', // Output directory
+  },
+});
