624bd1b0054f934c21de38e9edd49737a729c05a
Nelson <favournelson82@gmail.com>
2024-12-02 13:24:21 +0100

    Fix script

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use absolute path
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
-  },
 });
